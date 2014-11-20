25781c3d3d17ef1d6906c4c67cbc3f1806b67131
unknown <stedan@MBP-STEDAN.saltsea.net>
2014-11-20 00:50:41 -0600

    Revert "Fixed the seed"
    This reverts commit 8349384494b5d9ee61e77586f0b84beb648500c6.

--- EatsAPI/EatsAPI.Models/Initialization/EatsDbInitializer.cs
+++ EatsAPI/EatsAPI.Models/Initialization/EatsDbInitializer.cs
@@ -22,7 +22,8 @@ namespace EatsAPI.Models.Initialization
 			foreach (Category category in _defaultCategories)
 				context.Categories.Add(category);
 
-			context.SaveChanges();
+			foreach (Review review in _defaultReviews)
+				context.Reviews.Add(review);
 
 			foreach (Restaurant restaurant in _defaultRestaurants)
 				context.Restaurants.Add(restaurant);
@@ -32,11 +33,8 @@ namespace EatsAPI.Models.Initialization
 			for (int i = 0; i < _defaultReviews.Count; i++)
 			{
 				var restaurantIndex = i % 5;
-				_defaultReviews[i].Restaurant = _defaultRestaurants.FirstOrDefault(r => r.RestaurantId == (restaurantIndex + 1));
+				_defaultRestaurants.ElementAt(restaurantIndex).RestaurantReviews.Add(_defaultReviews.ElementAt(i));
 			}
-
-			foreach (Review review in _defaultReviews)
-				context.Reviews.Add(review);
 
 			context.SaveChanges();
 
@@ -47,9 +45,9 @@ namespace EatsAPI.Models.Initialization
 		{
 			_defaultCategories = new List<Category>();
 
-			_defaultCategories.Add(new Category { CategoryId = 1, Name = "Close", Description = "Close to the office, good for quick eats" });
-			_defaultCategories.Add(new Category { CategoryId = 2, Name = "Healthy", Description = "Good healthy eats options" });
-			_defaultCategories.Add(new Category { CategoryId = 3, Name = "Cheap", Description = "Great inexpensive food options" });
+			_defaultCategories.Add(new Category() { CategoryId = 1, Name = "Close", Description = "Close to the office, good for quick eats" });
+			_defaultCategories.Add(new Category() { CategoryId = 2, Name = "Healthy", Description = "Good healthy eats options" });
+			_defaultCategories.Add(new Category() { CategoryId = 3, Name = "Cheap", Description = "Great inexpensive food options" });
 		}
 
 		private void SeedRestaurants()
@@ -116,7 +114,7 @@ namespace EatsAPI.Models.Initialization
 				PriceRangeMax = 15
 			});
 		}
-
+	
 		private void SeedReviews()
 		{
 			_defaultReviews = new List<Review>();
